9fbf4b24e4943db9098e9a4f66855c4c5f251d67
Alan <alanpieczonka@gmail.com>
2017-02-04 21:45:46 +0100

    Random background color and transition

--- js/script.js
+++ js/script.js
@@ -3,6 +3,38 @@ console.log("JS WORKS!");
 $(document).ready(function(){
     console.log("THE JQUERY WORKS TOO!");
 
+    var colors = [
+      "#1abc9c",
+      "#2ecc71",
+      "#3498db",
+      "#9b59b6",
+      "#34495e",
+      "#16a085",
+      "#27ae60",
+      "#2980b9",
+      "#8e44ad",
+      "#2c3e50",
+      "#f1c40f",
+      "#e67e22",
+      "#e74c3c",
+      "#f39c12",
+      "#d35400",
+      "#c0392b"
+    ];
+
+
+    function changeColor(){
+      var randomnumber = Math.floor((Math.random() * 17));
+      console.log(randomnumber);
+      $('body').css('background-color', colors[randomnumber] );
+      $('.tweet').css('background-color', colors[randomnumber]);
+    };
+
+
+
+
+
+
     function getNewQuote(){
       $.ajax({ //making an API request
         url: 'http://api.forismatic.com/api/1.0/',
@@ -42,8 +74,8 @@ $(document).ready(function(){
 
 
   $(".yo").on('click', function() {
-
     getNewQuote();
+    changeColor();
   });
 
 
