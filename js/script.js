421ddf85dbcaa09f8b5107dea2d6fd3d3d8a2344
Alan <alanpieczonka@gmail.com>
2017-02-04 19:41:51 +0100

    Logic is ready

--- js/script.js
+++ js/script.js
@@ -17,9 +17,21 @@ $(document).ready(function(){
           console.log(response.quoteText);
           console.log(response.quoteAuthor);
           $(".myQuote").text(response.quoteText);
-          $(".author").text(response.quoteAuthor);
+          //$(".author").text(response.quoteAuthor);
+          quote = response.quoteText;
+          if(response.quoteAuthor){
+            author = response.quoteAuthor;
+            $(".author").text(response.quoteAuthor);
+          }
+          else{
+            author = "Unknown author";
+            $(".author").text("Unknown author");
+
+          }
 
 
+          //$(this).attr('href', 'https://twitter.com/intent/tweet?text=' + quote);
+          $(".twitter-share-button").attr("href", "https://twitter.com/intent/tweet?text=" + quote + "-- " + author);
         }
       });
 
@@ -28,14 +40,13 @@ $(document).ready(function(){
 
     getNewQuote();
 
-    $( "button" ).click(function() {
-  getNewQuote();;
-});
+
+  $(".yo").on('click', function() {
+
+    getNewQuote();
+  });
 
 
-    function tweetQuote(quoteText, quoteAuthor){
-      $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + quoteText);
 
-    };
 
 });
