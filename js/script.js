b5c9b104c16fc2ef9d6f4b472c48efb162f9083b
Alan <alanpieczonka@gmail.com>
2017-02-04 16:55:35 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+console.log("JS WORKS!");
+
+$(document).ready(function(){
+    console.log("THE JQUERY WORKS TOO!");
+
+    function getNewQuote(){
+      $.ajax({ //making an API request
+        url: 'http://api.forismatic.com/api/1.0/',
+        jsonp: 'jsonp', //becouse chrome doesn't allow normal json in this situation
+        dataType: 'jsonp',
+        data: {
+          method: 'getQuote',
+          lang: 'en',
+          format: 'jsonp'
+        },
+        success: function(response){
+          console.log(response.quoteText);
+          console.log(response.quoteAuthor);
+          $(".myQuote").text(response.quoteText);
+          $(".author").text(response.quoteAuthor);
+        }
+
+      });
+    }
+
+
+    getNewQuote();
+});
